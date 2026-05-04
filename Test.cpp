651f24e4685c80e4e4df970e193cbabff422e8c7
jieyyy06 <jie108011@gmail.com>
2026-05-04 16:43:59 +0800

    commitMessage

---
+++ Test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void foo(){
+    cout<<"It's foo!"<<endl;
+}
+
+void bar(){
+    cout<<"It's bar!!"<<endl;
+}
+
+int main(){
+    cout<<"Hello, World"<<endl;
+    return 0;
+}
